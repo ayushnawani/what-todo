210ca654e78764121b3a0394f230780408d0f992
Ayush Nawani <ayush.nawani@armentum.co>
2020-04-12 04:45:11 +0530

    Updates

---
+++ precache-manifest.2c7fe243730df8bcd09a5cc446a25040.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f39a819d1aee80fcd91633ca3005b8",
+    "url": "/what-todo/index.html"
+  },
+  {
+    "revision": "3c90630d46684e7247eb",
+    "url": "/what-todo/static/css/main.10f7d32c.chunk.css"
+  },
+  {
+    "revision": "c377dc8220443678a210",
+    "url": "/what-todo/static/js/2.6d3e9c91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/what-todo/static/js/2.6d3e9c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c90630d46684e7247eb",
+    "url": "/what-todo/static/js/main.7fce9e87.chunk.js"
+  },
+  {
+    "revision": "ed8ff94689223eba357d",
+    "url": "/what-todo/static/js/runtime-main.0b1ce731.js"
+  },
+  {
+    "revision": "274fcec95201beccae411f9078fb5bde",
+    "url": "/what-todo/static/media/bored.274fcec9.jpg"
+  },
+  {
+    "revision": "71401916b65cb63cec2fbc4fbefc987f",
+    "url": "/what-todo/static/media/chinup.71401916.jpg"
+  },
+  {
+    "revision": "fd0dc752445ec3a5675c73a3008cf047",
+    "url": "/what-todo/static/media/coffee.fd0dc752.jpg"
+  },
+  {
+    "revision": "9844d220b70f300be1603d5b56738801",
+    "url": "/what-todo/static/media/jogging.9844d220.png"
+  },
+  {
+    "revision": "5e86ffcac34fa3135befbed7f4ff3c76",
+    "url": "/what-todo/static/media/paneer.5e86ffca.jpg"
+  },
+  {
+    "revision": "e689973603230c7f343cc6cddaec8d74",
+    "url": "/what-todo/static/media/pasta.e6899736.jpeg"
+  },
+  {
+    "revision": "2da6893007bca5490bad881aabb30a0c",
+    "url": "/what-todo/static/media/pushup.2da68930.jpg"
+  }
+]);
